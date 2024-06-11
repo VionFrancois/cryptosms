ab9550a467b4a9d78b9d35242a53c0e12a1bb27d
frix <210809@umons.ac.be>
2024-06-11 15:21:44 +0200

    Clean models

--- lib/models/chatmessage_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-// TODO : Move this ?
-class ChatMessage{
-  String messageContent;
-  String messageType;
-  String date;
-  ChatMessage({required this.messageContent, required this.messageType, required this.date});
-}
